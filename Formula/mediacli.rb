01a7e44d44f1559fe76bf68268720179fdad9e01
copey02 <copey02@users.noreply.github.com>
2025-12-28 15:23:22 -0600

    Allow using TMDB IDs for mediacli rename

--- Formula/mediacli.rb
+++ Formula/mediacli.rb
@@ -1,9 +1,9 @@
 class Mediacli < Formula
   desc "Media CLI - Rename and organize movies & TV shows using TMDB (FileBot alternative)"
   homepage "https://github.com/copey02/homebrew-tap"
-  version "1.2.3"
+  version "1.2.4"
   url "https://raw.githubusercontent.com/copey02/homebrew-tap/v#{version}/bin/mediacli"
-  sha256 "3ebd28580d2eb05be8919478ad7939a69ea969a8f45a3d72224fbfd6b9fc6460"
+  sha256 "3e16dfbb2052e4c5276db7e744eb0cbe34b98ee09025a14eb2e5dd82bbb33a57"
   license "MIT"
 
   depends_on "python@3"
